5909cfee24ea0063d57380e5061233daf27fd8d4
Sedin-Mithun <mithun@sedintechnologies.com>
2024-07-30 12:19:18 +0530

    vercel url added to config and configured trpc integration

--- client/utils/trpc.ts
+++ client/utils/trpc.ts
@@ -3,18 +3,14 @@ import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 
 function getBaseUrl() {
-  if (typeof window !== 'undefined')
-    return '';
-
-  if (process.env.VERCEL_URL)
-    // reference for vercel.com
+  if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
+  }
 
-  if (process.env.RENDER_INTERNAL_HOSTNAME)
-    // reference for render.com
+  if (process.env.RENDER_INTERNAL_HOSTNAME) {
     return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+  }
 
-  // assume localhost
   return `http://localhost:${process.env.PORT ?? 4000}`;
 }
 
@@ -23,8 +19,7 @@ export const trpc = createTRPCNext<AppRouter>({
     return {
       links: [
         httpBatchLink({
-          url: "http://localhost:4000/trpc/",
-
+          url: `${getBaseUrl()}/trpc`,
         }),
       ],
     };
